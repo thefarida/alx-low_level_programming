c7fc8e9eb4d5f6ea69aa6a2159b23d315a56a574
thefarida <adamuibrahimfarida@gmail.com>
2022-07-01 15:14:43 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- *   * main - Prints a text
- *     *
- *       * Return: Always 0 (Success)
- *         */
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programing is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
