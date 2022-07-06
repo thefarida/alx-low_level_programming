7a07ceeb9a2ffda5606357df4b9e75dcee58ca8a
thefarida <adamuibrahimfarida@gmail.com>
2022-07-06 23:19:34 +0100

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print-alphabet -> prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+int f;
+for (f = 'a'; f <= 'z'; f++)
+{
+_putchar(f);
+}
+_putchar('\n');
+}
