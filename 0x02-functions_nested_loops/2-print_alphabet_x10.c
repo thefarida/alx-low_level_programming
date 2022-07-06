bc7c603bfc65cdf86659759efec6101fbd33c172
thefarida <adamuibrahimfarida@gmail.com>
2022-07-06 23:33:50 +0100

    Print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+int f;
+int x;
+
+for (x = 0; x < 10; x++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+
+}
