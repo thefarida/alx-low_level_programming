aaadb664fef167e1b0c6cf22184070aef2930d87
thefarida <adamuibrahimfarida@gmail.com>
2022-07-07 01:22:18 +0100

    24 Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer -> Prints 24 hours
+ *Return: ...
+*/
+void jack_bauer(void)
+{
+int w, x, y, z;
+
+for (w = 0; w <= 2; w++)
+{
+if ((w <= 1 && x <= 9) || (w <= 2 && x <= 3))
+{
+for (y = 0; y <= 5; y++)
+{
+for (z = 0; z <= 9; z++)
+{
+{
+_putchar(w + '0');
+_putchar(x + '0');
+_putchar(58);
+_putchar(y + '0');
+_putchar(z + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
+}
