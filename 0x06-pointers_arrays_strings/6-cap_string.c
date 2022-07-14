0663138300a39fc9090aac1450c57aea2a2d1e13
thefarida <adamuibrahimfarida@gmail.com>
2022-07-14 18:15:37 +0100

    Hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,11 +17,8 @@ char *cap_string(char *s)
 		i = 0;
 		while (i < len)
 		{
-			if
-			{
-				((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] = s[a] - 32;
-			}
+			if ((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
 			i++;
 		}
 		a++;
