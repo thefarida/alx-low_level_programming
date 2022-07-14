1bba4c0fccf1507bc9346bd2151ce602f4b6800d
thefarida <adamuibrahimfarida@gmail.com>
2022-07-14 18:59:49 +0100

    Capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
 			{
 				s[a] -= 32;
 			}
