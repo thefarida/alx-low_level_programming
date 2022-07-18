01c91e8a11926548aab56ba562c195257f6974f6
thefarida <adamuibrahimfarida@gmail.com>
2022-07-18 17:01:26 +0100

    Prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard -> Prints a chessboard
+ * @a: Array of pieces
+ *
+ * Return: Void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+
+	}
+}
